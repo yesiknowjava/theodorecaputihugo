bb3fa7565983f5fdcd6bf8c2984707fa36367d29
tlcaputi <tcaputi@gmail.com>
2021-06-10 19:54:23 -0400

    update submodule for new papers

--- citation-update.sh
+++ citation-update.sh
@@ -4,6 +4,9 @@ cd ..
 
 cd static/files
 xelatex -interaction=nonstopmode TheodoreCaputiShortCV.tex
+sleep 5
+xelatex -interaction=nonstopmode TheodoreCaputiShortCV.tex
+sleep 5
 xelatex -interaction=nonstopmode TheodoreCaputiShortCV.tex
 cd ..
 cd ..
@@ -13,10 +16,10 @@ hugo
 rm public/assets/css/main.scss
 cd public
 git add .
-git commit -m "update site"
+git commit -m "update site for new papers"
 git push
 
 cd ..
 git add .
-git commit -m "update submodule"
+git commit -m "update submodule for new papers"
 git push
